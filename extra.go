a65910ca13c290ca58690973cea48e463cea7ce1
Marc-François Cochaux-Laberge <mfcochauxlaberge@gmail.com>
2017-02-19 19:16:34 -0500

    Remove extra.go

--- extra.go
+++
@@ -1,8 +0,0 @@
-package jsonapi
-
-// An Extra struct contains all the side information found in a JSON API
-// document. This means the jsonapi, meta, and links top-level members.
-type Extra struct {
-	Meta    map[string]interface{}
-	JSONAPI map[string]interface{}
-}
