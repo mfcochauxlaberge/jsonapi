95ccd79f604949a506131c384feda63839f06e4d
Marc-François Cochaux-Laberge <mfcochauxlaberge@gmail.com>
2017-03-06 11:23:28 -0500

    Add two mock types for testing
    It includes a mock registry too.

---
+++ mock_registry.go
@@ -0,0 +1,65 @@
+package jsonapi
+
+import "time"
+
+// NewMockRegistry ...
+func NewMockRegistry() *Registry {
+	reg := NewRegistry()
+
+	reg.RegisterType(MockType1{})
+	reg.RegisterType(MockType2{})
+
+	return reg
+}
+
+// MockType1 ...
+type MockType1 struct {
+	ID string `json:"id" api:"mocktypes1"`
+
+	// Attributes
+	Str    string    `json:"str" api:"attr"`
+	Int    int       `json:"int" api:"attr"`
+	Int8   int8      `json:"int8" api:"attr"`
+	Int16  int16     `json:"int16" api:"attr"`
+	Int32  int32     `json:"int32" api:"attr"`
+	Int64  int64     `json:"int64" api:"attr"`
+	Uint   uint      `json:"uint" api:"attr"`
+	Uint8  uint8     `json:"uint8" api:"attr"`
+	Uint16 uint16    `json:"uint16" api:"attr"`
+	Uint32 uint32    `json:"uint32" api:"attr"`
+	Bool   bool      `json:"bool" api:"attr"`
+	Time   time.Time `json:"time" api:"attr"`
+
+	// Relationships
+	ToOne          string   `json:"to-one" api:"rel,mocktypes2"`
+	ToOneFromOne   string   `json:"to-one-from-one" api:"rel,mocktypes2,to-one-from-one"`
+	ToOneFromMany  string   `json:"to-one-from-many" api:"rel,mocktypes2,to-many-from-one"`
+	ToMany         []string `json:"to-many" api:"rel,mocktypes2"`
+	ToManyFromOne  []string `json:"to-many-from-one" api:"rel,mocktypes2,to-one-from-many"`
+	ToManyFromMany []string `json:"to-many-from-many" api:"rel,mocktypes2,to-many-from-many"`
+}
+
+// MockType2 ...
+type MockType2 struct {
+	ID string `json:"id" api:"mocktypes2"`
+
+	// Attributes
+	StrPtr    *string    `json:"str" api:"attr"`
+	IntPtr    *int       `json:"int" api:"attr"`
+	Int8Ptr   *int8      `json:"int8" api:"attr"`
+	Int16Ptr  *int16     `json:"int16" api:"attr"`
+	Int32Ptr  *int32     `json:"int32" api:"attr"`
+	Int64Ptr  *int64     `json:"int64" api:"attr"`
+	UintPtr   *uint      `json:"uint" api:"attr"`
+	Uint8Ptr  *uint8     `json:"uint8" api:"attr"`
+	Uint16Ptr *uint16    `json:"uint16" api:"attr"`
+	Uint32Ptr *uint32    `json:"uint32" api:"attr"`
+	BoolPtr   *bool      `json:"bool" api:"attr"`
+	TimePtr   *time.Time `json:"time" api:"attr"`
+
+	// Relationships
+	ToOneFromOne   string   `json:"to-one-from-one" api:"rel,mocktypes1,to-one-from-one"`
+	ToOneFromMany  string   `json:"to-one-from-many" api:"rel,mocktypes1,to-many-from-one"`
+	ToManyFromOne  []string `json:"to-many-from-one" api:"rel,mocktypes1,to-one-from-many"`
+	ToManyFromMany []string `json:"to-many-from-many" api:"rel,mocktypes1,to-many-from-many"`
+}
