70e291cb3a52fd844ad774bfd8bcc35c35bf26c0
Marc-François Cochaux-Laberge <mfcochauxlaberge@gmail.com>
2017-03-15 23:17:42 -0400

    Add test for parseParams

--- url_test.go
+++ url_test.go
@@ -187,31 +187,52 @@ func TestParseParams(t *testing.T) {
 				Include:      [][]Rel{},
 			},
 			expectedError: false,
+		}, {
+			// 2
+			url: `
+				?include=
+					to-many-from-one.to-one-from-many.to-one.to-many-from-many,
+					to-one-from-one.to-many-from-many
+				&sort=to-many,str,,-bool
+				&page[number]=3
+				&sort=uint8
+				&include=
+					to-many-from-one,
+					to-many-from-many
+				&page[size]=50
+			`,
+			resType: "mocktypes1",
+			expectedParams: Params{
+				Fields: map[string][]string{
+					"mocktypes1": reg.Types["mocktypes1"].Fields,
+					"mocktypes2": reg.Types["mocktypes2"].Fields,
+				},
+				Attrs:        map[string][]Attr{},
+				Rels:         map[string][]Rel{},
+				RelData:      map[string][]string{},
+				AttrFilters:  map[string]AttrFilter{},
+				RelFilters:   map[string]RelFilter{},
+				SortingRules: []string{"str", "-bool", "uint8"},
+				PageSize:     50,
+				PageNumber:   3,
+				Include: [][]Rel{
+					[]Rel{
+						reg.Types["mocktypes1"].Rels["to-many-from-many"],
+					},
+					[]Rel{
+						reg.Types["mocktypes1"].Rels["to-many-from-one"],
+						reg.Types["mocktypes2"].Rels["to-one-from-many"],
+						reg.Types["mocktypes1"].Rels["to-one"],
+						reg.Types["mocktypes2"].Rels["to-many-from-many"],
+					},
+					[]Rel{
+						reg.Types["mocktypes1"].Rels["to-one-from-one"],
+						reg.Types["mocktypes2"].Rels["to-many-from-many"],
+					},
+				},
+			},
+			expectedError: false,
 		},
-		// 	// 2
-		// 	url: `
-		// 		?include=
-		// 			to-many-from-one.to-one-from-many.to-one.to-many,
-		// 			to-one-from-one.to-many-from-many
-		// 		&include=
-		// 			to-many-from-one,
-		// 			to-many-from-many
-		// 	`,
-		// 	resType: "mocktypes1",
-		// 	expectedParams: Params{
-		// 		Fields:       map[string][]string{},
-		// 		Attrs:        map[string][]Attr{},
-		// 		Rels:         map[string][]Rel{},
-		// 		RelData:      map[string][]string{},
-		// 		AttrFilters:  map[string]AttrFilter{},
-		// 		RelFilters:   map[string]RelFilter{},
-		// 		SortingRules: []string{},
-		// 		PageSize:     0,
-		// 		PageNumber:   0,
-		// 		Include:      [][]Rel{},
-		// 	},
-		// 	expectedError: false,
-		// },
 	}
 
 	for n, test := range tests {
@@ -221,7 +242,24 @@ func TestParseParams(t *testing.T) {
 		params, err := parseParams(reg, test.resType, u)
 		tchek.ErrorExpected(t, n, test.expectedError, err)
 
+		// Set Attrs and Rels
+		for resType, fields := range test.expectedParams.Fields {
+			for _, field := range fields {
+				if res, ok := reg.Types[resType]; ok {
+					if _, ok := res.Attrs[field]; ok {
+						test.expectedParams.Attrs[resType] = append(test.expectedParams.Attrs[resType], res.Attrs[field])
+					} else if _, ok := reg.Types[resType].Rels[field]; ok {
+						test.expectedParams.Rels[resType] = append(test.expectedParams.Rels[resType], res.Rels[field])
+					}
+				}
+			}
+		}
+
 		if !test.expectedError {
+			// data, _ := json.MarshalIndent(test.expectedParams, "", "\t")
+			// fmt.Printf("EXPECTED:\n%s\n", data)
+			// data, _ = json.MarshalIndent(params, "", "\t")
+			// fmt.Printf("PROVIDED:\n%s\n", data)
 			tchek.AreEqual(
 				t, n,
 				test.expectedParams,
