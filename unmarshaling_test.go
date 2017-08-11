a81865086c4f1f4d0ae7c476a39c146baff35495
Marc-François Cochaux-Laberge <mfcochauxlaberge@gmail.com>
2017-08-11 18:55:16 -0400

    Remove TestUnmarshalCollection
    The server will never have to unmarshal a collection. At least with the
    current specification.

--- unmarshaling_test.go
+++ unmarshaling_test.go
@@ -41,32 +41,3 @@ func TestUnmarshalResource(t *testing.T) {
 	tchek.HaveEqualAttributes(t, -1, res1, dst1)
 	tchek.AreEqual(t, -1, meta1, pl1.Meta)
 }
-
-func TestUnmarshalCollection(t *testing.T) {
-	reg := NewMockRegistry()
-
-	col1 := WrapCollection(Wrap(&MockType1{}))
-	url1, err := ParseRawURL(reg, "/mocktypes1")
-	tchek.UnintendedError(err)
-
-	meta1 := map[string]interface{}{
-		"str": "a string\\^ç\"",
-		"num": float64(42),
-		"b":   true,
-	}
-
-	doc1 := NewDocument()
-	doc1.Data = col1
-	doc1.Meta = meta1
-
-	body1, err := Marshal(doc1, url1)
-	tchek.UnintendedError(err)
-
-	dst1 := WrapCollection(Wrap(&MockType1{}))
-
-	pl1, err := Unmarshal(body1, url1, reg)
-	tchek.UnintendedError(err)
-
-	tchek.HaveEqualAttributes(t, -1, col1, dst1)
-	tchek.AreEqual(t, -1, meta1, pl1.Meta)
-}
