cb0089e1b5051de3c6a3428749cf284e6c301aff
Marc-François Cochaux-Laberge <dev@mfcl.io>
2019-04-21 11:54:37 -0400

    Add comments

--- request.go
+++ request.go
@@ -5,7 +5,10 @@ import (
 	"net/http"
 )
 
-// NewRequest ...
+// NewRequest builds a return a *Request based on r and schema.
+//
+// schema can be nil, in which case no checks will be done to insure that
+// the request respects a specific schema.
 func NewRequest(r *http.Request, schema *Schema) (*Request, error) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -32,7 +35,7 @@ func NewRequest(r *http.Request, schema *Schema) (*Request, error) {
 	return req, nil
 }
 
-// Request ...
+// Request represents a JSON:API request.
 type Request struct {
 	Method string
 	URL    *URL
