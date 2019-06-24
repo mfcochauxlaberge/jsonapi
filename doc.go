f3286bdb961f1d22232438ebc203c11e8fd8777b
Marc-François Cochaux-Laberge <dev@mfcl.io>
2019-06-24 15:29:05 -0400

    Reformat doc.go

---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+Package jsonapi offers a library to marshal and unmarshal JSON:API payloads.
+
+It also offers many utilies for developing a JSON:API backend.
+*/
+package jsonapi
--- docs.go
+++
@@ -1,4 +0,0 @@
-// Package jsonapi offers a library to marshal and unmarshal JSON:API payloads.
-//
-// It also offers many utilies for developing a JSON:API backend.
-package jsonapi
