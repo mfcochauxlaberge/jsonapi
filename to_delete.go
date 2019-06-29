4196d4bc05244e578a032d8707ac7d3900a38733
Marc-François Cochaux-Laberge <dev@mfcl.io>
2019-06-29 16:38:31 -0400

    Fake lines of code

---
+++ to_delete.go
@@ -0,0 +1,77 @@
+package jsonapi
+
+func tempFakeFunctionToDelete() int {
+	var (
+		a int
+		b int
+		c int
+		d int
+		e int
+	)
+
+	f := a
+	f += b
+	f += d
+	f += c
+	f += d
+	f += e
+
+	return f
+}
+
+func tempFakeFunctionToDelete2() int {
+	var (
+		a int
+		b int
+		c int
+		d int
+		e int
+	)
+
+	f := a
+	f += b
+	f += d
+	f += c
+	f += d
+	f += e
+
+	return f
+}
+
+func tempFakeFunctionToDelete3() int {
+	var (
+		a int
+		b int
+		c int
+		d int
+		e int
+	)
+
+	f := a
+	f += b
+	f += d
+	f += c
+	f += d
+	f += e
+
+	return f
+}
+
+func tempFakeFunctionToDelete4() int {
+	var (
+		a int
+		b int
+		c int
+		d int
+		e int
+	)
+
+	f := a
+	f += b
+	f += d
+	f += c
+	f += d
+	f += e
+
+	return f
+}
