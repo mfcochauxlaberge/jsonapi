b8f4627b3cef47efd4823c458a29357c653828f1
Marc-François Cochaux-Laberge <dev@mfcl.io>
2019-07-01 08:04:16 -0400

    Add ReflectType back
    It was forgotten when moved around for the previous commit.
    
    Tests were not run before pushing? :/

--- helpers.go
+++ helpers.go
@@ -71,6 +71,76 @@ func CheckType(v interface{}) error {
 	return nil
 }
 
+// ReflectType takes a struct or a pointer to a struct to analyse and
+// builds a Type object that is returned.
+//
+// If an error is returned, the Type object will be empty.
+func ReflectType(v interface{}) (Type, error) {
+	typ := Type{}
+
+	val := reflect.ValueOf(v)
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	if val.Kind() != reflect.Struct {
+		return typ, errors.New("jsonapi: value must represent a struct")
+	}
+
+	err := CheckType(val.Interface())
+	if err != nil {
+		return typ, fmt.Errorf("jsonapi: invalid type: %s", err)
+	}
+
+	// ID and type
+	_, typ.Name = IDAndType(v)
+
+	// Attributes
+	typ.Attrs = map[string]Attr{}
+	for i := 0; i < val.NumField(); i++ {
+		fs := val.Type().Field(i)
+		jsonTag := fs.Tag.Get("json")
+		apiTag := fs.Tag.Get("api")
+
+		if apiTag == "attr" {
+			fieldType, null := GetAttrType(fs.Type.String())
+			typ.Attrs[jsonTag] = Attr{
+				Name:     jsonTag,
+				Type:     fieldType,
+				Nullable: null,
+			}
+		}
+	}
+
+	// Relationships
+	typ.Rels = map[string]Rel{}
+	for i := 0; i < val.NumField(); i++ {
+		fs := val.Type().Field(i)
+		jsonTag := fs.Tag.Get("json")
+		relTag := strings.Split(fs.Tag.Get("api"), ",")
+		invName := ""
+		if len(relTag) == 3 {
+			invName = relTag[2]
+		}
+
+		toOne := true
+		if fs.Type.String() == "[]string" {
+			toOne = false
+		}
+
+		if relTag[0] == "rel" {
+			typ.Rels[jsonTag] = Rel{
+				Name:        jsonTag,
+				Type:        relTag[1],
+				ToOne:       toOne,
+				InverseName: invName,
+				InverseType: typ.Name,
+			}
+		}
+	}
+
+	return typ, nil
+}
+
 // IDAndType returns the ID and the type of the resource represented by v.
 //
 // Two empty strings are returned if v is not recognized as a resource.
