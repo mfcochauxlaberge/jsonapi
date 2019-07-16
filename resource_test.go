8dc1d8d90c131e1e69d35f12536578bb44caccb7
Marc-François Cochaux-Laberge <dev@mfcl.io>
2019-07-15 23:57:55 -0400

    Add tests for Equal and EqualStrict

--- resource_test.go
+++ resource_test.go
@@ -100,9 +100,73 @@ func TestEqual(t *testing.T) {
 		ToManyFromMany: []string{"a", "b", "c"},
 	})
 
-	assert.True(Equal(mt11, mt11), "compare same resource with itself")
-	assert.True(Equal(mt11, mt12), "compare two identical resources")
-	assert.False(EqualStrict(mt11, mt12), "compare two identical resources (different IDs)")
-	assert.False(Equal(mt11, mt13), "compare two different resources")
-	assert.False(Equal(mt11, mt21), "compare resources of different types")
+	assert.True(Equal(mt11, mt11), "same instance")
+	assert.True(Equal(mt11, mt12), "identical resources")
+	assert.False(EqualStrict(mt11, mt12), "different IDs")
+	assert.False(Equal(mt11, mt13), "different resources (same type)")
+	assert.False(Equal(mt11, mt21), "different types")
+
+	typ := CopyType(mt11.GetType())
+	sr1 := &SoftResource{Type: &typ}
+	sr1.RemoveField("str")
+	assert.False(Equal(mt11, sr1), "different number of attributes")
+
+	typ = CopyType(mt11.GetType())
+	sr1 = &SoftResource{Type: &typ}
+	for _, attr := range typ.Attrs {
+		sr1.Set(attr.Name, mt11.Get(attr.Name))
+	}
+	for _, rel := range typ.Rels {
+		if rel.ToOne {
+			sr1.SetToOne(rel.Name, mt11.GetToOne(rel.Name))
+		} else {
+			sr1.SetToMany(rel.Name, mt11.GetToMany(rel.Name))
+		}
+	}
+	sr1.RemoveField("to-one")
+	assert.False(Equal(mt11, sr1), "different number of relationships")
+
+	sr1.AddRel(Rel{
+		Name:  "to-one",
+		Type:  "mocktypes2",
+		ToOne: false,
+	})
+	assert.False(Equal(mt11, sr1), "different to-one property")
+
+	sr1.RemoveField("to-one")
+	sr1.AddRel(Rel{
+		Name:  "to-one",
+		Type:  "mocktypes2",
+		ToOne: true,
+	})
+	sr1.SetToOne("to-one", "b")
+	assert.False(Equal(mt11, sr1), "different relationship value (to-one)")
+
+	sr1.SetToOne("to-one", "a")
+	sr1.SetToMany("to-many", []string{"d", "e", "f"})
+	assert.False(Equal(mt11, sr1), "different relationship value (to-many)")
+}
+
+func TestEqualStrict(t *testing.T) {
+	assert := assert.New(t)
+
+	sr1 := &SoftResource{}
+	sr1.SetType(&Type{
+		Name: "type",
+	})
+	sr2 := &SoftResource{}
+	sr2.SetType(&Type{
+		Name: "type",
+	})
+
+	// Same ID
+	sr1.SetID("an-id")
+	sr2.SetID("an-id")
+	assert.True(Equal(sr1, sr2))
+	assert.True(EqualStrict(sr1, sr2))
+
+	// Different ID
+	sr2.SetID("another-id")
+	assert.True(Equal(sr1, sr2))
+	assert.False(EqualStrict(sr1, sr2))
 }
