afdbcdc4b955530d2918a10ec5fa0efd6e152683
Marc-François Cochaux-Laberge <dev@mfcl.io>
2019-07-25 22:20:48 -0400

    Remove makeOneLine (unused)

--- util_test.go
+++ util_test.go
@@ -5,20 +5,6 @@ import (
 	"time"
 )
 
-func makeOneLine(str string) string {
-	str = strings.TrimSpace(str)
-	str = strings.Replace(str, "\t", " ", -1)
-	str = strings.Replace(str, "\n", " ", -1)
-
-	for {
-		str2 := strings.Replace(str, "  ", " ", -1)
-		if str == str2 {
-			return str
-		}
-		str = str2
-	}
-}
-
 func makeOneLineNoSpaces(str string) string {
 	str = strings.Replace(str, "\t", "", -1)
 	str = strings.Replace(str, "\n", "", -1)
