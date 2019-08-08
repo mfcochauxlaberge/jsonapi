e36dc0df0fc6dcd3af5cf001241e48980aaa9a94
Marc-François Cochaux-Laberge <dev@mfcl.io>
2019-08-07 22:28:22 -0400

    Save

--- jsonapi_test.go
+++ jsonapi_test.go
@@ -17,13 +17,29 @@ import (
 func TestMarshaling(t *testing.T) {
 	update := false
 
-	paths := []string{
+	// TODO Describe how this test suite works
+
+	// Schema
+	schema := getBaseSchema()
+
+	// Scenarios
+	collections := []*SoftCollection{
+		getEmptyBaseCollection("type1"),
+	}
+
+	urls := []string{
 		"/type1",
 		"/type1/t1-1",
 		"/type1/t1-1/to-1",
 		"/type1/t1-1/relationships/to-1",
 	}
 
+	lengths := []int{
+		len(collections),
+		len(urls),
+	}
+
+	// Test struct
 	tests := []struct {
 		name   string
 		schema *Schema
@@ -31,18 +47,36 @@ func TestMarshaling(t *testing.T) {
 		url    string
 	}{}
 
-	for _, path := range paths {
+	counter := make([]int, len(lengths))
+	run := true
+	for run {
+		// Add test
 		tests = append(tests, struct {
 			name   string
 			schema *Schema
 			col    *SoftCollection
 			url    string
 		}{
+			// TODO Give a different name to each test
 			name:   "some name",
-			schema: getBaseSchema(),
-			col:    getEmptyBaseCollection("type1"),
-			url:    path,
+			schema: schema,
+			col:    collections[0],
+			url:    urls[counter[1]],
 		})
+
+		// Increment counter
+		for i := 0; i < len(counter); i++ {
+			counter[i]++
+			if counter[i] == lengths[i] {
+				counter[i] = 0
+				if i == len(counter)-1 {
+					run = false
+					break
+				} else {
+					counter[i+1]++
+				}
+			}
+		}
 	}
 
 	for i := range tests {
@@ -199,127 +233,127 @@ type type3 struct {
 	Rel2 []string `json:"rel2" api:"rel,type1"`
 }
 
-var (
-	mocktypes1  Collection
-	mocktypes11 Collection
-	mocktypes2  Collection
-	mocktypes21 Collection
-	mocktypes3  Collection
-	// urls  []*URL
-)
+// var (
+// 	mocktypes1  Collection
+// 	mocktypes11 Collection
+// 	mocktypes2  Collection
+// 	mocktypes21 Collection
+// 	mocktypes3  Collection
+// 	// urls  []*URL
+// )
 
-func init() {
-	loc, _ := time.LoadLocation("")
+// func init() {
+// 	loc, _ := time.LoadLocation("")
 
-	// Resources
-	mocktypes1 = WrapCollection(Wrap(&mockType1{}))
-	mocktypes1.Add(
-		Wrap(&mockType1{
-			ID: "mt1-1",
-			// Use default (zero) value for each attribute
-		}),
-	)
-	mocktypes1.Add(
-		Wrap(&mockType1{
-			ID:     "mt1-2",
-			Str:    "",
-			Int:    -42,
-			Int8:   80,
-			Int16:  160,
-			Int32:  320,
-			Int64:  6464640000,
-			Uint:   42,
-			Uint8:  8,
-			Uint16: 1600,
-			Uint32: 32000,
-			Uint64: 64000,
-			Bool:   false,
-			Time:   time.Date(2017, 1, 2, 3, 4, 5, 6, loc),
-		}),
-	)
+// 	// Resources
+// 	mocktypes1 = WrapCollection(Wrap(&mockType1{}))
+// 	mocktypes1.Add(
+// 		Wrap(&mockType1{
+// 			ID: "mt1-1",
+// 			// Use default (zero) value for each attribute
+// 		}),
+// 	)
+// 	mocktypes1.Add(
+// 		Wrap(&mockType1{
+// 			ID:     "mt1-2",
+// 			Str:    "",
+// 			Int:    -42,
+// 			Int8:   80,
+// 			Int16:  160,
+// 			Int32:  320,
+// 			Int64:  6464640000,
+// 			Uint:   42,
+// 			Uint8:  8,
+// 			Uint16: 1600,
+// 			Uint32: 32000,
+// 			Uint64: 64000,
+// 			Bool:   false,
+// 			Time:   time.Date(2017, 1, 2, 3, 4, 5, 6, loc),
+// 		}),
+// 	)
 
-	mocktypes2 = WrapCollection(Wrap(&mockType2{}))
-	mocktypes2.Add(
-		Wrap(&mockType2{
-			ID: "mt2-1",
-			// Use nil values
-		}),
-	)
-	strPtr := "str"
-	intPtr := int(-42)
-	int8Ptr := int8(80)
-	int16Ptr := int16(160)
-	int32Ptr := int32(320)
-	int64Ptr := int64(6464640000)
-	uintPtr := uint(42)
-	uint8Ptr := uint8(8)
-	uint16Ptr := uint16(1600)
-	uint32Ptr := uint32(32000)
-	uint64Ptr := uint64(64000)
-	boolPtr := false
-	timePtr := time.Date(2017, 1, 2, 3, 4, 5, 6, loc)
-	mocktypes2.Add(
-		Wrap(&mockType2{
-			ID:        "mt1-2",
-			StrPtr:    &strPtr,
-			IntPtr:    &intPtr,
-			Int8Ptr:   &int8Ptr,
-			Int16Ptr:  &int16Ptr,
-			Int32Ptr:  &int32Ptr,
-			Int64Ptr:  &int64Ptr,
-			UintPtr:   &uintPtr,
-			Uint8Ptr:  &uint8Ptr,
-			Uint16Ptr: &uint16Ptr,
-			Uint32Ptr: &uint32Ptr,
-			Uint64Ptr: &uint64Ptr,
-			BoolPtr:   &boolPtr,
-			TimePtr:   &timePtr,
-		}),
-	)
+// 	mocktypes2 = WrapCollection(Wrap(&mockType2{}))
+// 	mocktypes2.Add(
+// 		Wrap(&mockType2{
+// 			ID: "mt2-1",
+// 			// Use nil values
+// 		}),
+// 	)
+// 	strPtr := "str"
+// 	intPtr := int(-42)
+// 	int8Ptr := int8(80)
+// 	int16Ptr := int16(160)
+// 	int32Ptr := int32(320)
+// 	int64Ptr := int64(6464640000)
+// 	uintPtr := uint(42)
+// 	uint8Ptr := uint8(8)
+// 	uint16Ptr := uint16(1600)
+// 	uint32Ptr := uint32(32000)
+// 	uint64Ptr := uint64(64000)
+// 	boolPtr := false
+// 	timePtr := time.Date(2017, 1, 2, 3, 4, 5, 6, loc)
+// 	mocktypes2.Add(
+// 		Wrap(&mockType2{
+// 			ID:        "mt1-2",
+// 			StrPtr:    &strPtr,
+// 			IntPtr:    &intPtr,
+// 			Int8Ptr:   &int8Ptr,
+// 			Int16Ptr:  &int16Ptr,
+// 			Int32Ptr:  &int32Ptr,
+// 			Int64Ptr:  &int64Ptr,
+// 			UintPtr:   &uintPtr,
+// 			Uint8Ptr:  &uint8Ptr,
+// 			Uint16Ptr: &uint16Ptr,
+// 			Uint32Ptr: &uint32Ptr,
+// 			Uint64Ptr: &uint64Ptr,
+// 			BoolPtr:   &boolPtr,
+// 			TimePtr:   &timePtr,
+// 		}),
+// 	)
 
-	mocktypes3 = WrapCollection(Wrap(&mockType3{}))
-	mocktypes3.Add(
-		Wrap(&mockType3{
-			ID: "mt3-1",
-		}),
-	)
-	mocktypes3.Add(
-		Wrap(&mockType3{
-			ID:    "mt3-1",
-			Attr1: "str",
-			Attr2: 32,
-		}),
-	)
+// 	mocktypes3 = WrapCollection(Wrap(&mockType3{}))
+// 	mocktypes3.Add(
+// 		Wrap(&mockType3{
+// 			ID: "mt3-1",
+// 		}),
+// 	)
+// 	mocktypes3.Add(
+// 		Wrap(&mockType3{
+// 			ID:    "mt3-1",
+// 			Attr1: "str",
+// 			Attr2: 32,
+// 		}),
+// 	)
 
-	mocktypes11 = WrapCollection(Wrap(&mockType1{}))
-	mocktypes11.Add(
-		Wrap(&mockType1{
-			ID:             "mt11-1",
-			ToOne:          "mt21-1",
-			ToMany:         []string{"mt21-2", "mt21-3"},
-			ToOneFromOne:   "",
-			ToManyFromMany: []string{},
-			ToManyFromOne:  []string{},
-		}),
-	)
+// 	mocktypes11 = WrapCollection(Wrap(&mockType1{}))
+// 	mocktypes11.Add(
+// 		Wrap(&mockType1{
+// 			ID:             "mt11-1",
+// 			ToOne:          "mt21-1",
+// 			ToMany:         []string{"mt21-2", "mt21-3"},
+// 			ToOneFromOne:   "",
+// 			ToManyFromMany: []string{},
+// 			ToManyFromOne:  []string{},
+// 		}),
+// 	)
 
-	mocktypes21 = WrapCollection(Wrap(&mockType2{}))
-	mocktypes21.Add(
-		Wrap(&mockType2{
-			ID:      "mt21-1",
-			BoolPtr: &boolPtr,
-		}),
-	)
-	mocktypes21.Add(
-		Wrap(&mockType2{
-			ID:     "mt21-2",
-			IntPtr: &intPtr,
-		}),
-	)
-	mocktypes21.Add(
-		Wrap(&mockType2{
-			ID:     "mt21-3",
-			StrPtr: &strPtr,
-		}),
-	)
-}
+// 	mocktypes21 = WrapCollection(Wrap(&mockType2{}))
+// 	mocktypes21.Add(
+// 		Wrap(&mockType2{
+// 			ID:      "mt21-1",
+// 			BoolPtr: &boolPtr,
+// 		}),
+// 	)
+// 	mocktypes21.Add(
+// 		Wrap(&mockType2{
+// 			ID:     "mt21-2",
+// 			IntPtr: &intPtr,
+// 		}),
+// 	)
+// 	mocktypes21.Add(
+// 		Wrap(&mockType2{
+// 			ID:     "mt21-3",
+// 			StrPtr: &strPtr,
+// 		}),
+// 	)
+// }
