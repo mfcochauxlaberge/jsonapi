0e6cac79e226d5ceec65f15f16d788aeeb22d85f
Marc-François Cochaux-Laberge <dev@mfcl.io>
2019-09-08 22:34:37 -0400

    Remove call to GetZeroValue in SoftResource.Get
    It can be assumed that the value exists in the data field because
    of the call of the check method.

--- soft_resource.go
+++ soft_resource.go
@@ -96,11 +96,10 @@ func (sr *SoftResource) GetType() Type {
 // Get returns the value associated to the field named after key.
 func (sr *SoftResource) Get(key string) interface{} {
 	sr.check()
-	if attr, ok := sr.Type.Attrs[key]; ok {
+	if _, ok := sr.Type.Attrs[key]; ok {
 		if v, ok := sr.data[key]; ok {
 			return v
 		}
-		return GetZeroValue(attr.Type, attr.Nullable)
 	}
 	return nil
 }
