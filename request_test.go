704e38973eab58cfcd32739c97ace5689768a69d
Marc-François Cochaux-Laberge <dev@mfcl.io>
2019-10-20 19:46:04 -0400

    Add tests for NewRequest

--- request_test.go
+++ request_test.go
@@ -2,6 +2,7 @@ package jsonapi_test
 
 import (
 	"bytes"
+	"errors"
 	"net/http/httptest"
 	"testing"
 
@@ -21,14 +22,20 @@ func TestNewRequest(t *testing.T) {
 		method        string
 		url           string
 		schema        *Schema
-		expectedError error
+		expectedError string
 	}{
 		{
 			name:          "get collection (mock schema)",
 			method:        "GET",
 			url:           "/mocktypes1",
 			schema:        schema,
-			expectedError: nil,
+			expectedError: "",
+		}, {
+			name:          "bad url",
+			method:        "GET",
+			url:           "/invalid",
+			schema:        schema,
+			expectedError: `400 Bad Request: "invalid" is not a known type.`,
 		},
 	}
 
@@ -37,7 +44,44 @@ func TestNewRequest(t *testing.T) {
 		req := httptest.NewRequest(test.method, test.url, body)
 
 		doc, err := NewRequest(req, test.schema)
-		assert.Equal(test.expectedError, err, test.name)
-		assert.Equal(test.method, doc.Method, test.name)
+		if test.expectedError == "" {
+			assert.NoError(err)
+			assert.Equal(test.method, doc.Method, test.name)
+		} else {
+			assert.EqualError(err, test.expectedError, test.name)
+			assert.Nil(doc)
+		}
 	}
+}
+
+func TestNewRequestInvalidBody(t *testing.T) {
+	assert := assert.New(t)
+
+	// Schema
+	schema := newMockSchema()
+
+	// Nil body
+	req := httptest.NewRequest("GET", "/mocktypes1", badReader{})
+
+	doc, err := NewRequest(req, schema)
+	assert.EqualError(err, "bad reader")
+	assert.Nil(doc)
+
+	// Invalid body
+	body := bytes.NewBufferString("{invalidjson}")
+	req = httptest.NewRequest("GET", "/mocktypes1", body)
+
+	doc, err = NewRequest(req, schema)
+	assert.EqualError(
+		err,
+		"invalid character 'i' looking for beginning of object key string",
+	)
+	assert.Nil(doc)
+}
+
+type badReader struct {
+}
+
+func (badReader) Read([]byte) (int, error) {
+	return 0, errors.New("bad reader")
 }
